a6e6d585bb7699c695686f264de11d76f5364aff
Eazyny <153327883+Eazyny@users.noreply.github.com>
2024-11-30 03:09:15 -0500

    Removed Extra Camera/Removed BedRoomCombo1.glb

--- src/index.jsx
+++ src/index.jsx
@@ -8,15 +8,7 @@ import Loader from './Loading';
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-  <Canvas
-    className="r3f"
-    camera={{
-      fov: 60,
-      near: 0.1,
-      far: 2000,
-      position: [-3, 1.5, 4],
-    }}
-  >
+  <Canvas className="r3f">
     <Suspense fallback={<Loader />}>
       <Experience />
     </Suspense>
